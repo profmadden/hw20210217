c304abfa07bc57342e3c27d0d520da84b796e4c7
Patrick Madden <pmadden@acm.org>
2021-02-17 09:57:52 -0500

    Initial generation of code

---
+++ gitswifthello/gitswifthello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitswifthello
+//
+//  Created by Patrick Madden on 2/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
